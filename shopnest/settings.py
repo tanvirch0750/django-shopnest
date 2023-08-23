b360f9495182ee37fba3b707645b8cab1117fc47
tanvirch0750 <tanvir0750@yahoo.com>
2023-08-23 20:27:42 +0600

    create accounts app and custom user model

--- shopnest/settings.py
+++ shopnest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'shopnest.urls'
+
+AUTH_USER_MODEL = 'accounts.Account'
 
 TEMPLATES = [
     {
