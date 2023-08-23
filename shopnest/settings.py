1708bdca9726f81b9dce9681fb33619994d5c71f
tanvirch0750 <tanvir0750@yahoo.com>
2023-08-23 21:24:39 +0600

    handling category model and media files

--- shopnest/settings.py
+++ shopnest/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'category'
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
